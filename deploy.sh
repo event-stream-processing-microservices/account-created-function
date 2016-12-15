ed636207ce45d2015ef48dc449ad659f0db4f54e
Kenny Bastani <kb@socialmoon.com>
2016-12-15 06:06:45 -1000

    Polish

--- deploy.sh
+++ deploy.sh
@@ -46,8 +46,8 @@ function package {
   echo ""
 
   aws cloudformation package \
-     --template-file account-created.yaml \
-     --output-template-file account-created-package.yaml \
+     --template-file package.yaml \
+     --output-template-file deployment.yaml \
      --s3-bucket $bucket_name || error_exit "Packaging failed: Could not access the S3 bucket..."
 
      echo ""
@@ -61,11 +61,11 @@ function deploy {
   echo ""
 
   aws cloudformation deploy \
-     --template-file account-created-package.yaml \
-     --stack-name lambda-stack || error_exit "Deployment failed..."
+     --template-file deployment.yaml \
+     --stack-name account-created || error_exit "Deployment failed..."
 
   # Remove the deployment package
-  rm ./account-created-package.yaml
+  rm ./deployment.yaml
 }
 
 package
